083c0e9225c296925c63e6e49f178efdf7879d36
Eemeli Ingervo <eemeli.ingervo@cgi.com>
2023-09-25 10:45:37 +0300

    Separated ping to a different file

--- serverless/src/handler.ts
+++ serverless/src/handler.ts
@@ -1,20 +1,6 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import validate from './validator'
 
-export const ping = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Pong!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  }
-}
-
 export const handlePost = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   if (event.body === null)
     return {
