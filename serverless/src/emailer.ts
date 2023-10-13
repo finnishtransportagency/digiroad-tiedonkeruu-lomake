b18bece46b2258e40875de5c99b010fc6f09d71f
Eemeli Ingervo <eemeli.ingervo@cgi.com>
2023-10-13 13:06:56 +0300

    Parsing SMTP credentials

--- serverless/src/emailer.ts
+++ serverless/src/emailer.ts
@@ -1,9 +1,26 @@
 import { getParameter } from './utils'
 
-const sendEmail = async () => {
-  const credentials = await getParameter(process.env.SES_CREDENTIALS_NAME || '')
+const getCredentials = async () => {
+  const credentialsString = await getParameter(process.env.SES_CREDENTIALS_NAME || '')
 
-  console.log(credentials)
+  const usernameRegex = /SMTP user name\s*([\s\S]+?)\r?\n/
+  const passwordRegex = /SMTP password\s*([\s\S]+)/
+  const usernameMatch = credentialsString.match(usernameRegex)
+  const passwordMatch = credentialsString.match(passwordRegex)
+
+  if (usernameMatch && passwordMatch) {
+    const username = usernameMatch[1].trim()
+    const password = passwordMatch[1].trim()
+
+    return { username, password }
+  } else {
+    throw new Error('Unable to parse credentials from SecureString')
+  }
+}
+
+const sendEmail = async () => {
+  const SMTP_credentials = await getCredentials()
+  console.log(SMTP_credentials.username, SMTP_credentials.password)
 }
 
 export default { sendEmail }
