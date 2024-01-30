1b98d4739e9a62ba0cb9b24bc5e5b1e05a451f3a
Jeemlei <eemeli.ingervo@cgi.com>
2024-01-30 11:55:23 +0200

    Added stage to the backend calls

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,4 @@
 export const apiURL = process.env.REACT_APP_API_URL ?? ''
 export const reCaptchaSiteKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY ?? ''
+export const stage = process.env.REACT_APP_STAGE ?? 'dev'
 
